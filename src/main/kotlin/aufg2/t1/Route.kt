510751b8ce7a4c036a279c9c1ac47dba9013b37e
mtraub <mario.traub@ergon.ch>
2026-05-18 09:19:32 +0200

    aufg. 2.1

--- src/main/kotlin/aufg2/t1/Route.kt
+++ src/main/kotlin/aufg2/t1/Route.kt
@@ -1,20 +1,10 @@
 package aufg2.t1
 
-class Route private constructor(
-    val destinations: List<String>
-) {
-    companion object {
-        fun createRoute(destinations: List<String>): Route = Route(destinations)
-    }
 
-    fun addDestination(destination: String) = Route(destinations + destination)
-    fun insertDestinationAtIndex(destination: String, index: Int) = Route(
-        destinations.subList(0, index)
-                + destination
-                + destinations.subList(index, destinations.size)
-    )
-    fun removeDestination(destination: String) = Route(destinations.filter { it != destination })
-    fun replaceDestination(destination: String, replacement: String) = Route(
-        destinations.map { if (it == destination) replacement else it }
-    )
-}
+fun addDestination(route: List<String>, destination: String) = route + destination
+fun insertDestinationAtIndex(route: List<String>, destination: String, index: Int) =
+    route.subList(0, index) + destination + route.subList(index, route.size)
+
+fun removeDestination(route: List<String>, destination: String) = route.filter { it != destination }
+fun replaceDestination(route: List<String>, destination: String, replacement: String) =
+    route.map { if (it == destination) replacement else it }
--- src/test/kotlin/aufg2/t1/RouteTest.kt
+++ src/test/kotlin/aufg2/t1/RouteTest.kt
@@ -1,52 +1,39 @@
 package aufg2.t1
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class RouteTest {
     val destList = listOf("A", "B", "C")
-    @Test
-    fun testCreation() {
-        val actual = Route.createRoute(destList).destinations
-        assertEquals(destList, actual)
-    }
 
     @Test
     fun testAddDestination() {
-        val subject = Route.createRoute(destList)
-
         val expected = destList + "D"
-        val actual = subject.addDestination("D").destinations
+        val actual = addDestination(destList, "D")
 
         assertEquals(expected, actual)
     }
 
     @Test
     fun testInsertDestinationAtIndex() {
-        val subject = Route.createRoute(destList)
-
         val expected = listOf("A", "D", "B", "C")
-        val actual = subject.insertDestinationAtIndex("D", 1).destinations
+        val actual = insertDestinationAtIndex(destList, "D", 1)
 
         assertEquals(expected, actual)
     }
 
     @Test
     fun testRemoveDestination() {
-        val subject = Route.createRoute(destList)
-
         val expected = listOf("A", "C")
-        val actual = subject.removeDestination("B").destinations
+        val actual = removeDestination(destList, "B")
 
         assertEquals(expected, actual)
     }
 
     @Test
     fun testReplaceDestination() {
-        val subject = Route.createRoute(destList)
-
         val expected = listOf("A", "D", "C")
-        val actual = subject.replaceDestination("B", "D").destinations
+        val actual = replaceDestination(destList, "B", "D")
 
         assertEquals(expected, actual)
     }
